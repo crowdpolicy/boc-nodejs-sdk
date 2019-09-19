73a355a246e78778ca12ab4683e2c7a3ec14d9ee
George Zaimis <geoza2000@gmail.com>
2019-09-19 13:39:25 +0300

    Add a complete UI to the example app

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ boc_api.init();
 
 app.use(cors());
 
+app.set('view engine', 'ejs');
+
 // Normal express config defaults
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,7 +28,6 @@ app.use(require('method-override')());
 app.use(express.static(__dirname + '/public'));
 
 app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
-app.set('view engine', 'pug')
 
 app.use("/", require("./routes/index"));
 app.use("/bocOauthcb", require("./routes/boc_callback"));
--- routes/index.js
+++ routes/index.js
@@ -1,78 +1,94 @@
 var router = require('express').Router();
 
 
-router.get('/',function(req,res,next){
-    if(boc_api && boc_api.subStatus.status === "ACTV" && boc_api.subStatus.selectedAccounts.length > 0){
-        res.send(JSON.stringify(boc_api))
-    }else{
-        res.send("<a href='"+boc_api.get_login_url()+"'>Connect a BOC Account</a>");
+router.get('/', function (req, res, next) {
+    if (boc_api) {
+        res.render('index', {
+            boc_api: boc_api
+        });
+    } else {
+        res.status(500).send('BoC SDK not initialized')
     }
-    
+    // if(boc_api && boc_api.subStatus.status === "ACTV" && boc_api.subStatus.selectedAccounts.length > 0){
+    //     res.send(JSON.stringify(boc_api))
+    // }else{
+    //     res.send("<a href='"+boc_api.get_login_url()+"'>Connect a BOC Account</a>");
+    // }
+
 })
 
-router.get("/getAccountsInSub", function(req,res,next){
-    if(req.query.accountId){
-        boc_api.getSubForAccount(req.query.accountId).then(subForAccount =>{
+router.get("/getAccountsInSub", function (req, res, next) {
+    if (req.query.accountId) {
+        boc_api.getSubForAccount(req.query.accountId).then(subForAccount => {
+            // res.render('accounts', {
+            //     subForAccount: subForAccount
+            // })
             res.send(subForAccount)
         })
-    }else{
-        res.send({error:"missing accountId"})
+    } else {
+        res.send({
+            error: "missing accountId"
+        })
     }
-   
+
 })
 
-router.get('/account/fundAvailability', function(req,res,next){
-    if(req.query.accountId){
-        boc_api.fundAvailability(req.query.accountId).then(response =>{
+router.get('/account/fundAvailability', function (req, res, next) {
+    if (req.query.accountId) {
+        boc_api.fundAvailability(req.query.accountId).then(response => {
             res.send(response)
         })
-    }else{
-        res.send({error:"missing accountId"})
-    }
-    
-})
-
-router.get('/account/statements', function(req,res,next){
-    if(req.query.accountId){
-        boc_api.getAccountStatements(req.query.accountId).then(accountStatements =>{
-            res.send(accountStatements)
+    } else {
+        res.send({
+            error: "missing accountId"
         })
-    }else{
-        res.send({error:"missing accountId"})
     }
-    
+
 })
 
-router.get('/account/balance', function(req,res,next){
-    if(req.query.accountId){
-        boc_api.getAvailBalanceForAccount(req.query.accountId).then(accountBalance =>{
-            res.send(accountBalance)
+router.get('/accounts/:accountid/statements', function (req, res, next) {
+    var account_id = req.params.accountid;
+    boc_api.getAccountStatements(account_id).then(accountStatements => {
+        res.render('statements', {
+            statements: accountStatements
         })
-    }else{
-        res.send({error:"missing accountId"})
-    }
-    
+        // res.send(accountStatements)
+    })
+
 })
 
-router.get('/accounts',function(req,res,next){
-    boc_api.getAccounts(function(err,data){
-        if(err){
+router.get('/accounts/:accountid/balances', function (req, res, next) {
+    var account_id = req.params.accountid;
+    boc_api.getAvailBalanceForAccount(account_id).then(accountBalance => {
+        res.render('balances', {
+            account: accountBalance[0]
+        })
+        // res.send(accountBalance)
+    })
+
+})
+
+router.get('/accounts', function (req, res, next) {
+    boc_api.getAccounts(function (err, data) {
+        if (err) {
             res.send(err)
-        }else{
+        } else {
             //res.send(data)
             res_obj = []
             data.forEach(account => {
-                boc_api.getAccount(account.accountId,function(err,accountData){
-                    if(err){
+                boc_api.getAccount(account.accountId, function (err, accountData) {
+                    if (err) {
                         throw err;
-                    }else{
-                        
+                    } else {
+
                         res_obj.push(accountData);
                     }
 
-                    if(res_obj.length === data.length){
-                        
-                        res.send(res_obj);
+                    if (res_obj.length === data.length) {
+                        res.render('accounts', {
+                            accounts: res_obj
+                        })
+                        // res.send(res_obj);
                     }
                 })
             });
@@ -80,105 +96,118 @@ router.get('/accounts',function(req,res,next){
     })
 })
 
-router.get('/accounts/:accountid',function(req,res,next){
+router.get('/accounts/:accountid', function (req, res, next) {
     var account_id = req.params.accountid;
-    boc_api.getAccount(account_id,function(err,accountData){
-        boc_api.getAccountPayments(account_id,function(err,paymentList){
-            var obj= {paymentlist: paymentList}
-            res.send(Object.assign(obj, accountData[0]))
+    boc_api.getAccount(account_id, function (err1, accountData) {
+        boc_api.getAccountPayments(account_id, function (err2, paymentList) {
+            var obj = {
+                paymentlist: paymentList
+            }
+            console.log(accountData)
+            if (!accountData.fatalError) {
+                res.render('account', {
+                    account: Object.assign(obj, accountData[0])
+                })
+            } else {
+                res.status(500).send('Error in accountId')
+            }
+
+            // res.send(Object.assign(obj, accountData[0]))
         })
     })
-    
+
 })
 
-router.get("/payment",function(req,res,next){
-    if(req.query.paymentId){
-        boc_api.getPaymentDetails(req.query.paymentId).then(paymentDetails =>{
-            res.send(paymentDetails)
+router.get("/payment/:paymentId", function (req, res, next) {
+    var payment_id = req.params.paymentId;
+    boc_api.getPaymentDetails(payment_id).then(paymentDetails => {
+        res.render('payment', {
+            payment: paymentDetails
         })
-    }else{
-        res.send({error:"missing paymentId"})
-    }
-
+    })
 })
 
-router.get('/pay',function(req,res,next){
-    boc_api.getAccounts(function(err,data){
-        if(err){
-            res.send(err)
-        }else{
-            console.log(data)
-            //first we sign the payment payload
-            if(data[0].accountId,data[1].accountId){
+router.get('/pay', function (req, res, next) {
+    if (req.query.creditorIban && req.query.debtorIban && req.query.amount) {
+        const creditorIban = req.query.creditorIban
+        const debtorIban = req.query.debtorIban
+        const amount = req.query.amount
 
-                
-                boc_api.signPaymentRequest(data[0].accountId,data[1].accountId,10,"SDK test payment",function(err,data){
-                    if(err){
+
+
+        boc_api.signPaymentRequest(creditorIban, debtorIban, amount, "SDK test payment", function (err, data) {
+            if (err) {
+                res.send(err)
+            } else {
+                //res.send(data)
+                boc_api.createPayment(data, function (err, paymentResult) {
+                    if (err) {
                         res.send(err)
-                    }else{
-                        //res.send(data)
-                        boc_api.createPayment(data,function(err,paymentResult){
-                            if(err){res.send(err)}
-                            else{
-                                
-                                boc_api.approvePayment(paymentResult.payment.paymentId,function(err,paymentAuthorizeResult){
-                                    if(err){res.send(err)}
-                                    else{
-                                        res.send(paymentAuthorizeResult)
-                                    }
+                    } else {
+
+                        console.log(paymentResult.payment.paymentId)
+                        boc_api.approvePayment(paymentResult.payment.paymentId, function (err, paymentAuthorizeResult) {
+                            if (err) {
+                                res.send(err)
+                            } else {
+                                res.render('pay', {
+                                    result: paymentAuthorizeResult
                                 })
+                                // res.send(paymentAuthorizeResult)
                             }
                         })
                     }
                 })
-            }else{
-                res.send({result:"error", message:"the API object must have access to atleast 2 accounts to perform this call. You will need to assign a accountId manually otherwise"})
             }
-            /*
-            var payload = {
-                "debtor": {
-                  "bankId": "",
-                  "accountId": "351012345671"
-                },
-                "creditor": {
-                  "bankId": "",
-                  "accountId": "351092345672"
-                },
-                "transactionAmount": {
-                  "amount": 666,
-                  "currency": "EUR",
-                  "currencyRate": "string"
-                },
-                "endToEndId": "string",
-                "paymentDetails": "yolo",
-                "terminalId": "string",
-                "branch": "",
-                 "executionDate": "",
-                "valueDate": ""
-              }
-              
-            boc_api.signPaymentRequest(payload,function(err,data){
-                if(err){
-                    res.send(err)
-                }else{
-                    boc_api.createPayment(data,function(err,paymentResult){
-                        if(err){res.send(err)}
-                        else{
-                            
-                            boc_api.approvePayment(paymentResult.payment.paymentId,function(err,paymentAuthorizeResult){
-                                if(err){res.send(err)}
-                                else{
-                                    res.send(paymentAuthorizeResult)
-                                }
-                            })
-                        }
-                    })
-                }
-            })*/
+        })
 
-        }
-    })
-    
+        /*
+        var payload = {
+            "debtor": {
+              "bankId": "",
+              "accountId": "351012345671"
+            },
+            "creditor": {
+              "bankId": "",
+              "accountId": "351092345672"
+            },
+            "transactionAmount": {
+              "amount": 666,
+              "currency": "EUR",
+              "currencyRate": "string"
+            },
+            "endToEndId": "string",
+            "paymentDetails": "yolo",
+            "terminalId": "string",
+            "branch": "",
+             "executionDate": "",
+            "valueDate": ""
+          }
+          
+        boc_api.signPaymentRequest(payload,function(err,data){
+            if(err){
+                res.send(err)
+            }else{
+                boc_api.createPayment(data,function(err,paymentResult){
+                    if(err){res.send(err)}
+                    else{
+                        
+                        boc_api.approvePayment(paymentResult.payment.paymentId,function(err,paymentAuthorizeResult){
+                            if(err){res.send(err)}
+                            else{
+                                res.send(paymentAuthorizeResult)
+                            }
+                        })
+                    }
+                })
+            }
+        })*/
+
+
+    } else {
+        res.status(400).send('missing information')
+    }
+
 })
 
 module.exports = router;
